d0358cd8fe260307cebbc9305c03cb498581afc7
Gádor <gador8@gmail.com>
2024-07-19 21:01:12 +0200

    Instalado e inicializado flask-jwt-extended, eliminado de user_model el comentario

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,21 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 
 from .utils.db_utils import bcrypt
+
 from .routes.user_route import user
 from .routes.product_route import product
 
 app = Flask(__name__)
+
+jwt = JWTManager()
 
 
 def run_app(config):
     app.config.from_object(config)
 
     bcrypt.init_app(app)
+    jwt.init_app(app)
 
     app.register_blueprint(user)
     app.register_blueprint(product)

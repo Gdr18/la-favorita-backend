84454d539a95d0dec1a6b0da55349c115594ca2e
Gádor <gador8@gmail.com>
2024-10-08 18:00:43 +0200

    Refactorizado test_exceptions_management.py

--- tests/tests_tools.py
+++ tests/tests_tools.py
@@ -1,10 +1,10 @@
-def validate_error_response(function, expected_status_code, expected_error_message):
-    response, status_code = function()
+def validate_error_response(function: tuple, expected_status_code: int, expected_error_message: str or list):
+    response, status_code = function
     assert status_code == expected_status_code
     assert response.get_json()['err'] == expected_error_message
 
 
-def validate_success_response(function, expected_status_code, expected_success_message):
+def validate_success_response(function: tuple, expected_status_code: int, expected_success_message: str):
     response, status_code = function
     assert status_code == expected_status_code
     assert response.get_json()['msg'] == expected_success_message
--- tests/tests_utils/test_exceptions_management.py
+++ tests/tests_utils/test_exceptions_management.py
@@ -5,7 +5,11 @@ from pymongo.errors import DuplicateKeyError
 from src.models.user_model import UserModel
 from src.models.product_model import ProductModel
 from src.utils.exceptions_management import ResourceNotFoundError, handle_validation_error, handle_duplicate_key_error, handle_unexpected_error
+from tests.tests_tools import validate_error_response
 from run import app as real_app
+
+
+code_validation_error = 400
 
 
 @pytest.fixture
@@ -16,10 +20,9 @@ def app():
 
 def test_resource_not_found(app):
     with app.app_context():
-        error = ResourceNotFoundError(1, "usuario")
-        response, status_code = error.json_response()
-        assert status_code == 404
-        assert response.get_json()["err"] == "El usuario con id 1 no ha sido encontrado"
+        function = ResourceNotFoundError(1, "usuario").json_response()
+        expected_error_message = "El usuario con id 1 no ha sido encontrado"
+        validate_error_response(function, 404, expected_error_message)
 
 
 # tests para manejar errores de campos no permitidos
@@ -28,9 +31,9 @@ def test_handle_validation_error_extra_inputs_are_not_permitted(app):
         try:
             UserModel(name="John Doe", email="john.doe@example.com", password="ValidPass!9", role=1, color="red", size="big")
         except ValidationError as error:
-            response, status_code = handle_validation_error(error)
-            assert status_code == 400
-            assert response.get_json()['err'] == "Hay 2 campos que no son válidos: 'color', 'size'."
+            function = handle_validation_error(error)
+            expected_error_message = "Hay 2 campos que no son válidos: 'color', 'size'."
+            validate_error_response(function, code_validation_error, expected_error_message)
 
 
 # Test para manejar errores de campos requeridos faltantes
@@ -39,9 +42,9 @@ def test_handle_validation_error_field_required(app):
         try:
             UserModel()
         except ValidationError as error:
-            response, status_code = handle_validation_error(error)
-            assert status_code == 400
-            assert response.get_json()['err'] == "Faltan 3 campos requeridos: 'name', 'email', 'password'."
+            function = handle_validation_error(error)
+            expected_error_msg = "Faltan 3 campos requeridos: 'name', 'email', 'password'."
+            validate_error_response(function, code_validation_error, expected_error_msg)
 
 
 # Test para manejar errores de tipos de datos incorrectos
@@ -50,9 +53,9 @@ def test_handle_validation_error_input_should_be(app):
         try:
             UserModel(name=234235, email="john.doe@example.com", password="ValidPass!9", role=1, phone=23252)
         except ValidationError as error:
-            response, status_code = handle_validation_error(error)
-            assert status_code == 400
-            assert response.get_json()['err'] == "El campo 'name' debe ser de tipo 'string'. El campo 'phone' debe ser de tipo 'string'."
+            function = handle_validation_error(error)
+            expected_error_message = "El campo 'name' debe ser de tipo 'string'. El campo 'phone' debe ser de tipo 'string'."
+            validate_error_response(function, code_validation_error, expected_error_message)
 
 
 # Test para manejar errores value error que necesitan formatearse
@@ -61,20 +64,20 @@ def test_handle_validation_error_value_error_formatting(app):
         try:
             UserModel(name="John Doe", email="john.doe@example.com", password="ValidPass", role=1)
         except ValidationError as error:
-            response, status_code = handle_validation_error(error)
-            assert status_code == 400
-            assert response.get_json()['err'] == "La contraseña debe tener al menos 8 caracteres, contener al menos una mayúscula, una minúscula, un número y un carácter especial (!@#$%^&*_-)"
+            function = handle_validation_error(error)
+            expected_error_message = "La contraseña debe tener al menos 8 caracteres, contener al menos una mayúscula, una minúscula, un número y un carácter especial (!@#$%^&*_-)"
+            validate_error_response(function, code_validation_error, expected_error_message)
 
 
-# Test para manejar errorres de listas con menos elementos de los requeridos
+# Test para manejar errores de listas con menos elementos de los requeridos
 def test_handle_validation_error_items_should_be_in_collection(app):
     with app.app_context():
         try:
             ProductModel(name="Tomato", stock=44, categories=[])
         except ValidationError as error:
-            response, status_code = handle_validation_error(error)
-            assert status_code == 400
-            assert response.get_json()['err'] == "El campo 'categories' debe ser de tipo 'list' con al menos 1 elemento."
+            function = handle_validation_error(error)
+            expected_error_message = "El campo 'categories' debe ser de tipo 'list' con al menos 1 elemento."
+            validate_error_response(function, code_validation_error, expected_error_message)
 
 
 # Test para manejar errores en el campo email
@@ -83,9 +86,9 @@ def test_handle_validation_error_invalid_email(app):
         try:
             UserModel(name="John Dale", email="john.doe@example", password="ValidPass!9", role=1)
         except ValidationError as error:
-            response, status_code = handle_validation_error(error)
-            assert status_code == 400
-            assert response.get_json()['err'] == "El email no es válido."
+            function = handle_validation_error(error)
+            expected_error_message = "El email no es válido."
+            validate_error_response(function, code_validation_error, expected_error_message)
 
 
 # Test para manejar errores de validación inesperados
@@ -95,9 +98,9 @@ def test_handle_validation_error_default_case(app, mocker):
         error_mock.errors.return_value = [
             {"msg": "Some other error"}
         ]
-        response, status_code = handle_validation_error(error_mock)
-        assert status_code == 400
-        assert response.get_json()["err"] == ["{'msg': 'Some other error'}"]
+        function = handle_validation_error(error_mock)
+        expected_error_message = ["{'msg': 'Some other error'}"]
+        validate_error_response(function, code_validation_error, expected_error_message)
 
 
 # Test para manejar errores de campos con valores duplicados
@@ -106,17 +109,15 @@ def test_handle_duplicate_key_error(app, mocker):
         error = DuplicateKeyError("E11000 duplicate key error collection: db.collection index: _id_ dup key: { _id: 1 }")
         mocker.patch('pymongo.errors.DuplicateKeyError.details', new_callable=mocker.PropertyMock, return_value={"keyValue": {"_id": "1"}})
 
-        response, status_code = handle_duplicate_key_error(error)
-
-        assert status_code == 409
-        assert response.get_json() == {"err": "Error de clave duplicada en MongoDB: {'_id': '1'}"}
+        function = handle_duplicate_key_error(error)
+        expected_error_message = "Error de clave duplicada en MongoDB: {'_id': '1'}"
+        validate_error_response(function, 409, expected_error_message)
 
 
 # Test para manejar errores inesperados
 def test_handle_unexpected_error(app):
     with app.app_context():
         error_message = "Test error"
-        response, status_code = handle_unexpected_error(Exception(error_message))
-
-        assert status_code == 500
-        assert response.get_json() == {"err": f"Ha ocurrido un error inesperado. {error_message}"}
+        function = handle_unexpected_error(Exception(error_message))
+        expected_error_message = f"Ha ocurrido un error inesperado. {error_message}"
+        validate_error_response(function, 500, expected_error_message)

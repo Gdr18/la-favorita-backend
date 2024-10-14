47a3a111b382037e66bef72cffefa59928dd7d47
Gádor <gador8@gmail.com>
2024-10-14 19:35:07 +0200

    Refactorizado test_setting_route y creadas funciones reutilizables en tests_tools

--- tests/tests_routes/test_setting_route.py
+++ tests/tests_routes/test_setting_route.py
@@ -1,15 +1,13 @@
 import pytest
-from bson import ObjectId
-from pymongo.errors import DuplicateKeyError
-from pydantic import ValidationError
 
 from src import app as real_app
-from src.utils.exceptions_management import ResourceNotFoundError
+from tests.tests_tools import request_adding_valid_resource, request_invalid_resource_duplicate_key_error, request_invalid_resource_validation_error, request_unexpected_error, request_getting_resources, request_getting_resource, request_resource_not_found, request_resource_not_found_error, request_updating_resource, request_deleting_resource
 
 
 URL_SETTING = '/setting'
 URL_SETTINGS = '/settings'
 URL_SETTING_ID = '/setting/507f1f77bcf86cd799439011'
+RESOURCE = 'setting'
 
 
 @pytest.fixture
@@ -40,145 +38,86 @@ def invalid_setting_data():
     return {'name': 12345, 'values': ['value1', 'value2']}
 
 
+@pytest.fixture
+def updated_setting_data():
+    return {'name': 'new_value'}
+
+
 def test_add_setting(client, mock_db, valid_setting_data):
-    mock_db.insert_one.return_value.inserted_id = ObjectId()
-    response = client.post(URL_SETTING, json=valid_setting_data)
-    assert response.status_code == 201
-    assert 'id' in response.json['msg']
+    return request_adding_valid_resource(client, mock_db, URL_SETTING, valid_setting_data)
 
 
-def test_add_setting_duplicate_key(client, mock_db, mocker, valid_setting_data):
-    error = DuplicateKeyError("E11000 duplicate key error")
-    mocker.patch('pymongo.errors.DuplicateKeyError.details', new_callable=mocker.PropertyMock, return_value={"keyValue": {"name": "test"}})
-    mock_db.insert_one.side_effect = error
-    response = client.post(URL_SETTING, json=valid_setting_data)
-    assert response.status_code == 409
-    assert 'err' in response.json
+def test_add_setting_duplicate_key(client, mock_db, mocker, valid_setting_data, updated_setting_data):
+    return request_invalid_resource_duplicate_key_error(client, mock_db, mocker, 'post', URL_SETTING, valid_setting_data, updated_setting_data, 'name')
 
 
 def test_add_setting_validation_error(client, mock_db, mocker, invalid_setting_data):
-    error = mocker.Mock(spec=ValidationError)
-    mock_db.insert_one.side_effect = error
-    response = client.post(URL_SETTING, json=invalid_setting_data)
-    assert response.status_code == 400
-    assert 'err' in response.json
+    return request_invalid_resource_validation_error(client, mock_db, mocker, 'post', URL_SETTING, invalid_setting_data)
 
 
-def test_add_setting_unexpected_error(client, mock_db, mocker):
-    mocker.patch('src.routes.setting_route.coll_settings.insert_one', side_effect=Exception("Unexpected Error"))
-    response = client.post(URL_SETTING)
-    assert response.status_code == 500
-    assert 'err' in response.json
+def test_add_setting_unexpected_error(client, mocker):
+    return request_unexpected_error(client, mocker, 'insert_one', URL_SETTING)
 
 
-def test_get_settings(client, mock_db):
-    mock_db.find.return_value = [{'key': 'value'}]
-    response = client.get(URL_SETTINGS)
-    assert response.status_code == 200
-    assert isinstance(response.json, list)
+def test_get_settings(client, mock_db, valid_setting_data):
+    return request_getting_resources(client, mock_db, URL_SETTINGS, valid_setting_data)
 
 
-def test_get_settings_unexpected_error(client, mock_db, mocker):
-    mocker.patch('src.routes.setting_route.coll_settings.find', side_effect=Exception("Unexpected Error"))
-    response = client.get(URL_SETTINGS)
-    assert response.status_code == 500
+def test_get_settings_unexpected_error(client, mocker):
+    return request_unexpected_error(client, mocker, 'find', URL_SETTINGS)
 
 
-def test_get_setting(client, mock_db):
-    mock_db.find_one.return_value = {'_id': ObjectId(), 'key': 'value'}
-    response = client.get(URL_SETTING_ID)
-    assert response.status_code == 200
-    assert 'key' in response.json
+def test_get_setting(client, mock_db, valid_setting_data):
+    return request_getting_resource(client, mock_db, URL_SETTING_ID, valid_setting_data)
 
 
-def test_get_setting_not_found(app, client, mock_db):
-    with real_app.app_context():
-        mock_db.find_one.return_value = None
-        response = client.get(URL_SETTING_ID)
-        assert response.status_code == 404
-        assert response.json['err'] == "El/la configuración con id '507f1f77bcf86cd799439011' no ha sido encontrado/a."
-
-        error = ResourceNotFoundError('507f1f77bcf86cd799439011', 'setting')
-        mock_db.find_one.side_effect = error
-        response = client.get(URL_SETTING_ID)
-        assert response.status_code == 404
-        assert response.json['err'] == "El/la setting con id '507f1f77bcf86cd799439011' no ha sido encontrado/a."
+def test_get_setting_resource_not_found(app, client, mock_db):
+    return request_resource_not_found(app, client, mock_db, 'get', URL_SETTING_ID)
 
 
-def test_get_setting_unexpected_error(client, mock_db, mocker):
-    mocker.patch('src.routes.setting_route.coll_settings.find_one', side_effect=Exception("Unexpected Error"))
-    response = client.get(URL_SETTING_ID)
-    assert response.status_code == 500
+def test_get_setting_resource_not_found_error(client, mock_db, valid_setting_data):
+    return request_resource_not_found_error(client, mock_db, 'get', URL_SETTING_ID, valid_setting_data, RESOURCE)
 
 
-def test_update_setting(client, mock_db):
-    mock_db.find_one.return_value = {'name': 'value', 'values': ['value1', 'value2']}
-    mock_db.find_one_and_update.return_value = {'name': 'new_value'}
-    response = client.put(URL_SETTING_ID, json={'name': 'new_value'})
-    assert response.status_code == 200
-    assert 'name' in response.json
+def test_get_setting_unexpected_error(client, mocker):
+    return request_unexpected_error(client, mocker, 'find_one', URL_SETTING_ID)
 
 
-def test_update_setting_not_found(client, mock_db):
-    with real_app.app_context():
-        mock_db.find_one.return_value = None
-        response = client.put(URL_SETTING_ID)
-        assert response.status_code == 404
-        assert response.json['err'] == "El/la configuración con id '507f1f77bcf86cd799439011' no ha sido encontrado/a."
-
-        error = ResourceNotFoundError('507f1f77bcf86cd799439011', 'setting')
-        mock_db.find_one.side_effect = error
-        response = client.put(URL_SETTING_ID)
-        assert response.status_code == 404
-        assert response.json['err'] == "El/la setting con id '507f1f77bcf86cd799439011' no ha sido encontrado/a."
+def test_update_setting(client, mock_db, updated_setting_data, valid_setting_data):
+    return request_updating_resource(client, mock_db, URL_SETTING_ID, valid_setting_data, updated_setting_data)
 
 
-def test_update_setting_duplicate_key(client, mock_db, mocker):
-    error = DuplicateKeyError("E11000 duplicate key error")
-    mocker.patch('pymongo.errors.DuplicateKeyError.details', new_callable=mocker.PropertyMock, return_value={"keyValue": {"name": "new_value"}})
-    mock_db.find_one.return_value = {'name': 'value', 'values': ['value1', 'value2']}
-    mock_db.find_one_and_update.side_effect = error
-    response = client.put(URL_SETTING_ID, json={'name': 'new_value'})
-    assert response.status_code == 409
-    assert response.json['err'] == "Error de clave duplicada en MongoDB: {'name': 'new_value'}"
+def test_update_setting_resource_not_found(app, client, mock_db):
+    return request_resource_not_found(app, client, mock_db, 'put', URL_SETTING_ID)
+
+
+def test_update_setting_resource_not_found_error(client, mock_db, valid_setting_data):
+    return request_resource_not_found_error(client, mock_db, 'put', URL_SETTING_ID, valid_setting_data, RESOURCE)
+
+
+def test_update_setting_duplicate_key(client, mock_db, mocker, updated_setting_data, valid_setting_data):
+    return request_invalid_resource_duplicate_key_error(client, mock_db, mocker, 'put', URL_SETTING_ID, valid_setting_data, updated_setting_data)
 
 
 def test_update_setting_validation_error(client, mock_db, mocker, invalid_setting_data):
-    error = mocker.Mock(spec=ValidationError)
-    mock_db.insert_one.side_effect = error
-    response = client.put(URL_SETTING_ID, json=invalid_setting_data)
-    assert response.status_code == 400
-    assert 'err' in response.json
+    return request_invalid_resource_validation_error(client, mock_db, mocker, 'put', URL_SETTING_ID, invalid_setting_data)
 
 
 def test_update_setting_unexpected_error(client, mock_db, mocker):
-    mocker.patch('src.routes.setting_route.coll_settings.find_one_and_update', side_effect=Exception("Unexpected Error"))
-    response = client.put(URL_SETTING_ID)
-    assert response.status_code == 500
+    return request_unexpected_error(client, mocker, 'find_one_and_update', URL_SETTING_ID)
 
 
 def test_delete_setting(client, mock_db):
-    mock_db.delete_one.return_value.deleted_count = 1
-    response = client.delete(URL_SETTING_ID)
-    assert response.status_code == 200
-    assert 'id' in response.json['msg']
+    return request_deleting_resource(client, mock_db, URL_SETTING_ID)
 
 
-def test_delete_setting_not_found(app, client, mock_db):
-    with real_app.app_context():
-        mock_db.delete_one.return_value.deleted_count = 0
-        response = client.delete(URL_SETTING_ID)
-        assert response.status_code == 404
-        assert response.json['err'] == "El/la configuración con id '507f1f77bcf86cd799439011' no ha sido encontrado/a."
+def test_delete_setting_resource_not_found(app, client, mock_db):
+    return request_resource_not_found(app, client, mock_db, 'delete', URL_SETTING_ID)
 
-        error = ResourceNotFoundError('507f1f77bcf86cd799439011', 'setting')
-        mock_db.delete_one.side_effect = error
-        response = client.delete(URL_SETTING_ID)
-        assert response.status_code == 404
-        assert response.json['err'] == "El/la setting con id '507f1f77bcf86cd799439011' no ha sido encontrado/a."
+
+def test_delete_setting_resource_not_found_error(client, mock_db, valid_setting_data):
+    return request_resource_not_found_error(client, mock_db, 'delete', URL_SETTING_ID, valid_setting_data, RESOURCE)
 
 
 def test_delete_setting_unexpected_error(client, mock_db, mocker):
-    mocker.patch('src.routes.setting_route.coll_settings.delete_one', side_effect=Exception("Unexpected Error"))
-    response = client.delete(URL_SETTING_ID)
-    assert response.status_code == 500
+    return request_unexpected_error(client, mocker, 'delete_one', URL_SETTING_ID)

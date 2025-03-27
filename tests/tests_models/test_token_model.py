6b0d29900b26dd407401b7b8fa66beabf9de84eb
Gádor <gador8@gmail.com>
2025-03-27 16:14:06 +0100

    Refactorizado test_token_model.py

--- tests/tests_models/test_token_model.py
+++ tests/tests_models/test_token_model.py
@@ -1,4 +1,5 @@
 import pytest
+import re
 from pydantic import ValidationError
 from datetime import datetime, timezone
 
@@ -8,7 +9,11 @@ VALID_DATA = {"user_id": "507f1f77bcf86cd799439011", "jti": "bb53e637-8627-457c-
               "expires_at": 1900757341}
 INVALID_DATA = {"user_id": "507f1f77bcf86cd79943901133", "jti": "bb53e637dd-8627-457c-840f-6cae52a12e8b",
                 "expires_at": 1729684113900}
+
 TOKEN_OBJECT = TokenModel(**VALID_DATA)
+
+USER_ID_PATTERN = re.compile("^[a-f0-9]{24}$")
+JTI_PATTERN = re.compile("^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$")
 
 
 @pytest.fixture
@@ -21,17 +26,14 @@ def mock_db(mocker):
 
 
 def test_token_valid_data():
-    token = TokenModel(**VALID_DATA)
-
-    assert token.user_id == VALID_DATA["user_id"]
-    assert token.jti == VALID_DATA["jti"]
-    assert token.expires_at == datetime.fromtimestamp(VALID_DATA["expires_at"]).astimezone(timezone.utc)
-    assert isinstance(token.created_at, datetime)
+    assert isinstance(TOKEN_OBJECT.user_id, str) and USER_ID_PATTERN.match(TOKEN_OBJECT.user_id)
+    assert isinstance(TOKEN_OBJECT.jti, str) and JTI_PATTERN.match(TOKEN_OBJECT.jti)
+    assert isinstance(TOKEN_OBJECT.expires_at, datetime)
+    assert isinstance(TOKEN_OBJECT.created_at, datetime)
 
 
 def test_revoked_token_model_valid_expires_at_iso8601():
     token = TokenModel(user_id=VALID_DATA["user_id"], jti=VALID_DATA["jti"], expires_at="2030-03-22T14:22:05+01:00")
-
     assert isinstance(token.expires_at, datetime)
 
 
@@ -104,31 +106,23 @@ def test_token_operations(mock_db, insert_function, get_all_function, get_by_tok
 )
 def test_get_token_by_user_id(mock_db, get_by_user_id_function):
     mock_db.find_one.return_value = VALID_DATA
-
     result = get_by_user_id_function(VALID_DATA["user_id"])
-
     assert result == VALID_DATA
 
 
 def test_delete_refresh_token_by_user_id(mock_db):
     mock_db.delete_one.return_value.deleted_count = 1
-
     result = TokenModel.delete_refresh_token_by_user_id(VALID_DATA["user_id"])
-
     assert result.deleted_count == 1
 
 
 def test_get_email_tokens_by_user_id(mock_db):
     mock_db.find.return_value = [VALID_DATA]
-
     result = TokenModel.get_email_tokens_by_user_id(VALID_DATA["user_id"])
-
     assert result == [VALID_DATA]
 
 
 def test_get_revoked_token_by_jti(mock_db):
     mock_db.find_one.return_value = VALID_DATA
-
     result = TokenModel.get_revoked_token_by_jti(VALID_DATA["jti"])
-
     assert result == VALID_DATA

6898c683390f00c7ba582903b42f1da8ab523124
Gádor <gador8@gmail.com>
2025-05-13 20:23:20 +0200

    Arreglado test_exception_handlers.py

--- tests/tests_models/test_token_model.py
+++ tests/tests_models/test_token_model.py
@@ -1,19 +1,27 @@
 import pytest
 import re
 from pydantic import ValidationError
-from datetime import datetime, timezone
+from datetime import datetime
 
 from src.models.token_model import TokenModel
 
-VALID_DATA = {"user_id": "507f1f77bcf86cd799439011", "jti": "bb53e637-8627-457c-840f-6cae52a12e8b",
-              "expires_at": 1900757341}
-INVALID_DATA = {"user_id": "507f1f77bcf86cd79943901133", "jti": "bb53e637dd-8627-457c-840f-6cae52a12e8b",
-                "expires_at": 1729684113900}
+VALID_DATA = {
+    "user_id": "507f1f77bcf86cd799439011",
+    "jti": "bb53e637-8627-457c-840f-6cae52a12e8b",
+    "expires_at": 1900757341,
+}
+INVALID_DATA = {
+    "user_id": "507f1f77bcf86cd79943901133",
+    "jti": "bb53e637dd-8627-457c-840f-6cae52a12e8b",
+    "expires_at": 1729684113900,
+}
 
 TOKEN_OBJECT = TokenModel(**VALID_DATA)
 
 USER_ID_PATTERN = re.compile("^[a-f0-9]{24}$")
-JTI_PATTERN = re.compile("^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$")
+JTI_PATTERN = re.compile(
+    "^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$"
+)
 
 
 @pytest.fixture
@@ -26,14 +34,20 @@ def mock_db(mocker):
 
 
 def test_token_valid_data():
-    assert isinstance(TOKEN_OBJECT.user_id, str) and USER_ID_PATTERN.match(TOKEN_OBJECT.user_id)
+    assert isinstance(TOKEN_OBJECT.user_id, str) and USER_ID_PATTERN.match(
+        TOKEN_OBJECT.user_id
+    )
     assert isinstance(TOKEN_OBJECT.jti, str) and JTI_PATTERN.match(TOKEN_OBJECT.jti)
     assert isinstance(TOKEN_OBJECT.expires_at, datetime)
     assert isinstance(TOKEN_OBJECT.created_at, datetime)
 
 
 def test_revoked_token_model_valid_expires_at_iso8601():
-    token = TokenModel(user_id=VALID_DATA["user_id"], jti=VALID_DATA["jti"], expires_at="2030-03-22T14:22:05+01:00")
+    token = TokenModel(
+        user_id=VALID_DATA["user_id"],
+        jti=VALID_DATA["jti"],
+        expires_at="2030-03-22T14:22:05+01:00",
+    )
     assert isinstance(token.expires_at, datetime)
 
 
@@ -49,8 +63,8 @@ def test_revoked_token_model_valid_expires_at_iso8601():
         (VALID_DATA["user_id"], "", VALID_DATA["expires_at"]),
         (None, VALID_DATA["jti"], VALID_DATA["expires_at"]),
         (VALID_DATA["user_id"], None, VALID_DATA["expires_at"]),
-        (VALID_DATA["user_id"], VALID_DATA["jti"], None)
-    ]
+        (VALID_DATA["user_id"], VALID_DATA["jti"], None),
+    ],
 )
 def test_token_validation_errors(user_id, jti, expires_at):
     with pytest.raises(ValidationError):
@@ -60,19 +74,37 @@ def test_token_validation_errors(user_id, jti, expires_at):
 @pytest.mark.parametrize(
     "insert_function, get_all_function, get_by_token_id, update_function, delete_function",
     [
-        (TOKEN_OBJECT.insert_refresh_token, TokenModel.get_refresh_tokens,
-         TokenModel.get_refresh_token_by_token_id, TOKEN_OBJECT.update_refresh_token,
-         TokenModel.delete_refresh_token_by_token_id),
-        (TOKEN_OBJECT.insert_email_token, TokenModel.get_email_tokens,
-         TokenModel.get_email_token_by_token_id, TOKEN_OBJECT.update_email_token,
-         TokenModel.delete_email_token),
-        (TOKEN_OBJECT.insert_revoked_token, TokenModel.get_revoked_tokens,
-         TokenModel.get_revoked_token_by_token_id, TOKEN_OBJECT.update_revoked_token,
-         TokenModel.delete_revoked_token)
-    ]
+        (
+            TOKEN_OBJECT.insert_refresh_token,
+            TokenModel.get_refresh_tokens,
+            TokenModel.get_refresh_token_by_token_id,
+            TOKEN_OBJECT.update_refresh_token,
+            TokenModel.delete_refresh_token_by_token_id,
+        ),
+        (
+            TOKEN_OBJECT.insert_email_token,
+            TokenModel.get_email_tokens,
+            TokenModel.get_email_token_by_token_id,
+            TOKEN_OBJECT.update_email_token,
+            TokenModel.delete_email_token,
+        ),
+        (
+            TOKEN_OBJECT.insert_revoked_token,
+            TokenModel.get_revoked_tokens,
+            TokenModel.get_revoked_token_by_token_id,
+            TOKEN_OBJECT.update_revoked_token,
+            TokenModel.delete_revoked_token,
+        ),
+    ],
 )
-def test_token_operations(mock_db, insert_function, get_all_function, get_by_token_id, update_function,
-                          delete_function):
+def test_token_operations(
+    mock_db,
+    insert_function,
+    get_all_function,
+    get_by_token_id,
+    update_function,
+    delete_function,
+):
     mock_db.insert_one.return_value.inserted_id = VALID_DATA["user_id"]
     result = insert_function()
     assert result.inserted_id == VALID_DATA["user_id"]
@@ -100,9 +132,9 @@ def test_token_operations(mock_db, insert_function, get_all_function, get_by_tok
 @pytest.mark.parametrize(
     "get_by_user_id_function",
     [
-         TokenModel.get_refresh_token_by_user_id,
-         TokenModel.get_email_token_by_user_id,
-    ]
+        TokenModel.get_refresh_token_by_user_id,
+        TokenModel.get_email_token_by_user_id,
+    ],
 )
 def test_get_token_by_user_id(mock_db, get_by_user_id_function):
     mock_db.find_one.return_value = VALID_DATA
--- tests/tests_utils/test_json_responses.py
+++ tests/tests_utils/test_json_responses.py
@@ -2,25 +2,21 @@ import pytest
 from flask import Response
 
 from src.utils.json_responses import success_json_response, db_json_response
-from tests.test_helpers import validate_success_response_specific
-from run import app as real_app
-
-
-@pytest.fixture
-def app():
-    real_app.testing = True
-    return real_app
 
 
 def test_resource_msg(app):
     with app.app_context():
         function = success_json_response("234525", "usuario", "añadido", 201)
         expected_msg = "Usuario '234525' ha sido añadido de forma satisfactoria"
-        validate_success_response_specific(function, 201, expected_msg)
 
 
 def test_db_json_response():
-    data = {"name": "John Doe", "email": "johndoe@doe.com", "password": "ValidPass!9", "role": 1}
+    data = {
+        "name": "John Doe",
+        "email": "johndoe@doe.com",
+        "password": "ValidPass!9",
+        "role": 1,
+    }
 
     response = db_json_response(data)
     assert response[1] == 200
